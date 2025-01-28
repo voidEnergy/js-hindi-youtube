59cd07cff9d3cbbd748842f5ad3e69ec1304373c
voidEnergy <rohit.ranjan08062006@gmail.com>
2025-01-28 11:45:43 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// comparison(>,<,>=,<=) and '==' work differently
+// in case of comparison operators, null gets converted into 0
+
+console.log(undefined == 0);
+console.log(undefined >= 0)
+console.log(undefined > 0);
+
+// === (strict check-> checks both values and data types)
+
+console.log("2" === 2);
+
+
