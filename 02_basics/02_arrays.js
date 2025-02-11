920e5286ba0025a301bad4fead61a66ee312646a
Rohit <rohit.ranjan08062006@gmail.com>
2025-02-11 06:01:27 +0000

    arrays2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,9 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+marvel_heroes.concat(dc)
